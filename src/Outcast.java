a39980a841288d76e4c12a930378358fe5000cff
nick serebrennikov <Nick.Serebrennikov@derivco.co.uk>
2017-10-29 20:00:19 +0000

    add run configurations

--- src/Outcast.java
+++ src/Outcast.java
@@ -1,3 +1,6 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Outcast {
     public Outcast(WordNet wordnet)         // constructor takes a WordNet object
     {
@@ -8,7 +11,13 @@ public class Outcast {
         return null;
     }
 
-    public static void main(String[] args)  // see test client below
-    {
+    public static void main(String[] args) {
+        WordNet wordnet = new WordNet(args[0], args[1]);
+        Outcast outcast = new Outcast(wordnet);
+        for (int t = 2; t < args.length; t++) {
+            In in = new In(args[t]);
+            String[] nouns = in.readAllStrings();
+            StdOut.println(args[t] + ": " + outcast.outcast(nouns));
+        }
     }
 }
--- src/SAP.java
+++ src/SAP.java
@@ -11,22 +11,22 @@ public class SAP {
 
     // length of shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w) {
-        return 0;
+        return -1;
     }
 
     // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
     public int ancestor(int v, int w) {
-        return 0;
+        return -1;
     }
 
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
     public int length(Iterable<Integer> v, Iterable<Integer> w) {
-        return 0;
+        return -1;
     }
 
     // a common ancestor that participates in shortest ancestral path; -1 if no such path
     public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-        return 0;
+        return -1;
     }
 
     // do unit testing of this class
